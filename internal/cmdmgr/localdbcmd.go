4e31576d02355e83b4c2a9cf3aae54acafc1fcc6
YoungJauSenior <daryushkari123@gmail.com>
2019-11-30 22:01:05 +0330

    fix bug in localdb command

--- internal/cmdmgr/localdbcmd.go
+++ internal/cmdmgr/localdbcmd.go
@@ -8,6 +8,12 @@ import (
 
 // LocaldbManage manages all commands starting with localdb and checks user input and calls related functions
 func LocaldbManage(inputCommands []string){
+
+	if len(inputCommands) < 3{
+		fmt.Println("too few arguments please enter more")
+		return
+	}
+
 	switch inputCommands[2] {
 
 	case "init":
